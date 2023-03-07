4771ea3fcf1d3e2974219a46e339585306a5a5c9
doomzp <voidpatino@gmail.com>
2023-03-07 00:59:33 -0500

    Pause is right, just gotta print in a prettier way

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 import sys
 import time
 import tty
-import termios
-import subprocess
 import multiprocessing
 
 def checkFile () -> None:
@@ -32,41 +30,58 @@ class Work:
         if not mins.isdigit(): self.__total = 1200
         else:                  self.__total = int(mins) * 60
 
-        # Information about the time, 0: How many seconds
-        # have passed by, 1: hours counter, 2: minutes counter
-        # and seconds counter.
+        # 'self.__info': Information about the time.
+        # idx 0: How many seconds have passed by.
+        # idx 1: hours counter.
+        # idx 2: minutes counter
+        # idx 3: seconds counter.
+        # This gotta be like this since parent and child threads
+        # doesn't share same memory location, since if one value
+        # changes in the parent thread, it'll not be affected on
+        # child one.
         self.__info     = multiprocessing.Manager().list([0, 0, 0, 0])
-        self.__twork    = multiprocessing.Process(target = self.__working)
         self.__prssdkey = 0
         self.__task     = task
-        self.__paused   = False
 
-        # TODO: ...
-        self.__tstts = termios.tcgetattr(sys.stdin)
-        tty.setcbreak(sys.stdin);
+        self.__countDown()
+
+        # This variable is an exception since it's shared by both threads.
+        self.__paused = multiprocessing.Event()
+        self.__twork  = multiprocessing.Process(target = self.__working)
 
         self.__twork.start()
-        while True:
-            if not self.__twork.is_alive():
-                break
+        tty.setcbreak(sys.stdin);
 
+        while self.__twork.is_alive():
             self.__prssdkey = sys.stdin.read(1)[0]
             if self.__prssdkey == chr(27):
                 self.__twork.terminate()
                 print("The program was stoped since ESC key was pressed.")
-                print(f"You worked {self.__info[1]}:{self.__info[2]}:{self.__info[3]} at '{self.__task}'. Congrats!")
                 break
 
+            # TODO: Print in a prettier way when the stopwatch is paused.
+            # if self.__prssdkey == chr(32):
+            #     if self.__paused.is_set():
+            #         self.__paused.clear()
+            #     else:
+            #         print("")
+            #         self.__paused.set()
+
+        print(f"You've worked {self.__info[1]}:{self.__info[2]}:{self.__info[3]} on '{self.__task}'. Congrats!")
         print("PROGRAM ENDED.")
 
     def __countDown (self) -> None:
-        for sec in range(2, -1, -1):
+        for sec in range(10, -1, -1):
             print(f"Getting started :: {sec} seconds left!", end = '\r')
-            time.sleep(1)
+            time.sleep(0.5)
         os.system("clear")
 
     def __working (self) -> None:
         while (self.__info[0] <= self.__total):
+            # if self.__paused.is_set():
+            #    print("IT'S PAUSED...", end = '\r')
+            #    time.sleep(10)
+
             if self.__info[3] == 60:
                 self.__info[2] += 1
                 self.__info[3] = 0
@@ -78,7 +93,7 @@ class Work:
             print(f"{self.__info[1]}:{self.__info[2]}:{self.__info[3]} :: Working on '{self.__task}' :: STAY HARD!", end = '\r')
             self.__info[3] += 1
             self.__info[0] += 1
-            time.sleep(1)
+            time.sleep(0.1)
 
         print("")
         print("WELL DONE!")
--- test.py
+++ test.py
@@ -1,21 +1,43 @@
-import multiprocessing
+from multiprocessing import Process, Event, Queue
+import time
 
-class X:
-    def __init__ (self):
-        self.__values = multiprocessing.Manager().list([0, 0, 0])
-        self.__procss = multiprocessing.Process(target = self.__do)
-        self.__procss.start()
+def worker(pause_event, stop_event, output_queue):
+    while not stop_event.is_set():
+        if not pause_event.is_set():
+            # Do some work here
+            output_queue.put("Working")
+        time.sleep(1)
 
-        while True:
-            inp = input(":")
-            if inp == 's':
-                break
-        print(self.__values)
+def main():
+    pause_event = Event()
+    stop_event = Event()
+    output_queue = Queue()
+    p = Process(target=worker, args=(pause_event, stop_event, output_queue))
+    p.start()
 
-    def __do (self):
-        self.__values[0] = 1
-        self.__values[1] = 2
-        self.__values[2] = 3
+    while True:
+        # Do some work in the main process here
+        time.sleep(1)
 
+        # Pause the worker process
+        pause_event.set()
 
-X()
+        # Wait for the worker to finish processing any pending work
+        while not output_queue.empty():
+            print(output_queue.get())
+
+        # Stop the worker process
+        stop_event.set()
+
+        # Wait for the worker to exit
+        p.join()
+
+        # Restart the worker process
+        pause_event.clear()
+        stop_event.clear()
+        output_queue = Queue()
+        p = Process(target=worker, args=(pause_event, stop_event, output_queue))
+        p.start()
+
+if __name__ == '__main__':
+    main()
